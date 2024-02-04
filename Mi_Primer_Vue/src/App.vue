e8efe22a4617e9106d5267cea0b881de5bc3a1ed
Andres Garcia <andresfgarciac96@gmail.com>
2024-02-04 11:42:00 -0500

    practice vue js

---
+++ Mi_Primer_Vue/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+// Constantes
+const name = "Vue variable";
+const arrayColors = ["blue", "red", "black"];
+const arrayRaces = ["Blanca", "Mestiza", "Negra"];
+const arrayCities = ["Colombiano", "Brasileño", "Argentino"];
+const arrayLanguages = ["Español", "Ingles", "Portugues", "Mandarin"];
+
+// Metodo
+const handlerClickParam = (message) => {
+  console.log(message);
+};
+const handlerClick = () => {
+  console.log("Click");
+};
+</script>
+
+<template>
+  <div>
+    <h1>Hola {{ name.toUpperCase() }}</h1>
+
+    <h2>{{ arrayColors }}</h2>
+
+    <h2 v-bind:style="`color:${arrayColors[2]}`">
+      Soy {{ arrayCities[2] }} de raza {{ arrayRaces[1] }}
+    </h2>
+
+    <ul>
+      <li v-for="(language, index) in arrayLanguages" :key="index">
+        {{ index }}->{{ language }}
+      </li>
+    </ul>
+
+    <button v-on:click="handlerClick">Activame</button>
+    <button @click="handlerClickParam('Parametro')">Activame 2</button>
+  </div>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
